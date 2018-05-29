6d576af859705661e49250cfebd9a06585a0d31e
Jesus Lizama <dev@hellojeb.us>
2018-05-28 22:03:09 -0700

    add mux and home handler

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package go_envoz_api
+package main
+
+import (
+	"github.com/gorilla/mux"
+	"fmt"
+	"net/http"
+)
+
+func main(){
+	r := mux.NewRouter()
+	r.HandleFunc("/", HomeHandler)
+}
+
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Home handler")
+}
