8c4edc613084414e2d1dadb4336a951068568fdb
Jesus Lizama <dev@hellojeb.us>
2018-05-28 22:05:36 -0700

    add bind to localhost

--- main.go
+++ main.go
@@ -4,11 +4,13 @@ import (
 	"github.com/gorilla/mux"
 	"fmt"
 	"net/http"
+	"log"
 )
 
 func main(){
 	r := mux.NewRouter()
 	r.HandleFunc("/", HomeHandler)
+	log.Fatal(http.ListenAndServe(":8000", r))
 }
 
 func HomeHandler(w http.ResponseWriter, r *http.Request) {
