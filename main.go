683e4299b2456eef4eab6c19917c55c3e13a66d0
Jesus Lizama <dev@hellojeb.us>
2018-05-31 22:32:55 -0700

    move vars into main func

--- main.go
+++ main.go
@@ -16,12 +16,6 @@ type User struct {
 	Name string `json:"name"`
 }
 
-//connect to db
-var dbHost string = os.Getenv("DB_HOST")
-var dbName string = os.Getenv("DB_NAME")
-var dbUser string = os.Getenv("DB_USERNAME")
-var dbPassword string = os.Getenv("DB_PASSWORD")
-
 //database global var error
 var db *gorm.DB
 var dbError error
@@ -34,7 +28,13 @@ func main(){
 	port := os.Getenv("PORT")
 	router := mux.NewRouter()
 
-	//connec to db
+	//connect to db
+	var dbHost string = os.Getenv("DB_HOST")
+	var dbName string = os.Getenv("DB_NAME")
+	var dbUser string = os.Getenv("DB_USERNAME")
+	var dbPassword string = os.Getenv("DB_PASSWORD")
+
+	//connect to db
 	db, dbError = gorm.Open("mysql", dbUser+":"+ dbPassword +"@tcp(" + dbHost+ ":3306)/"+ dbName + "?charset=utf8&parseTime=True&loc=Local")
 	if dbError != nil {
 		panic("Failed to connect to database")
