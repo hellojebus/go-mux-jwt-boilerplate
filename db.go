049502d3d6f16e3aadc1b606cd3deacb95227084
Jesus Lizama <dev@hellojeb.us>
2018-06-02 11:16:04 -0700

    refactor the db config and connection

--- db.go
+++ db.go
@@ -1,1 +1,37 @@
 package main
+
+import (
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/jinzhu/gorm"
+	"os"
+)
+
+//database global
+var DB *gorm.DB
+
+func SetupDB() *gorm.DB {
+
+	//db config vars
+	var dbHost string = os.Getenv("DB_HOST")
+	var dbName string = os.Getenv("DB_NAME")
+	var dbUser string = os.Getenv("DB_USERNAME")
+	var dbPassword string = os.Getenv("DB_PASSWORD")
+
+	//connect to db
+	db, dbError := gorm.Open("mysql", dbUser+":"+ dbPassword +"@tcp(" + dbHost+ ":3306)/"+ dbName + "?charset=utf8&parseTime=True&loc=Local")
+	if dbError != nil {
+		panic("Failed to connect to database")
+	}
+
+	//fix for connection timeout
+	//see: https://github.com/go-sql-driver/mysql/issues/257
+	db.DB().SetMaxIdleConns(0)
+
+	//handles model updates (no deletes or changes to existing columns)
+	db.AutoMigrate(&User{})
+
+	//defer connection
+	//defer db.Close()
+
+	return db
+}
--- main.go
+++ main.go
@@ -1,50 +1,20 @@
 package main
 
 import (
-	_ "github.com/jinzhu/gorm/dialects/mysql"
 	"os"
-	"github.com/jinzhu/gorm"
-	"github.com/gorilla/mux"
 	"log"
 	"net/http"
 )
-
-//database global var error
-var DB *gorm.DB
-var DBError error
 
 func main(){
 
 	//init router
 	port := os.Getenv("PORT")
-	router := mux.NewRouter()
+	router := NewRouter()
 
-	//db config vars
-	var dbHost string = os.Getenv("DB_HOST")
-	var dbName string = os.Getenv("DB_NAME")
-	var dbUser string = os.Getenv("DB_USERNAME")
-	var dbPassword string = os.Getenv("DB_PASSWORD")
-
-	//connect to db
-	DB, DBError = gorm.Open("mysql", dbUser+":"+ dbPassword +"@tcp(" + dbHost+ ":3306)/"+ dbName + "?charset=utf8&parseTime=True&loc=Local")
-	if DBError != nil {
-		panic("Failed to connect to database")
-	}
-
-	//fix for connection timeout
-	//see: https://github.com/go-sql-driver/mysql/issues/257
-	DB.DB().SetMaxIdleConns(0)
-
-	//defer connection
+	//Setup database
+	DB = SetupDB()
 	defer DB.Close()
-
-	//handles model updates (no deletes or changes to existing columns)
-	DB.AutoMigrate(&User{})
-
-	//routes
-	router.HandleFunc("/users", UsersHandler).Methods("GET")
-	router.HandleFunc("/users/{id}", UserHandler).Methods("GET")
-	router.HandleFunc("/users", CreateUserHandler).Methods("POST")
 
 	//create http server
 	log.Fatal(http.ListenAndServe(":"+port, router))
