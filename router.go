ae0ed63de808f98e8be094ded8e034329328c2e1
Jesus Lizama <dev@hellojeb.us>
2018-06-02 14:56:38 -0700

    refactor routes

--- router.go
+++ router.go
@@ -7,10 +7,13 @@ func NewRouter() *mux.Router {
 	//init router
 	router := mux.NewRouter()
 
-	//routes
-	router.HandleFunc("/users", UsersHandler).Methods("GET")
-	router.HandleFunc("/users/{id}", UserHandler).Methods("GET")
-	router.HandleFunc("/users", CreateUserHandler).Methods("POST")
+	for _, r := range routes {
+		router.
+			Methods(r.Method).
+			Path(r.Pattern).
+			Name(r.Name).
+			Handler(r.HandlerFunc)
+	}
 
 	return router
 }
