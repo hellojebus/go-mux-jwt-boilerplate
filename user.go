360bf4490ba76e881cf79d89621167a5a293d937
Jesus Lizama <dev@hellojeb.us>
2018-06-02 11:28:49 -0700

    add hash to user struct

--- user.go
+++ user.go
@@ -4,6 +4,7 @@ type User struct {
 	ID   int `gorm:"primary_key" json:"id"`
 	Email string `gorm:"unique_index" json:"email"`
 	Name string `json:"name"`
+	Hash string `json:"-"`
 }
 
 func (u User) hashPassword(password string) string {
