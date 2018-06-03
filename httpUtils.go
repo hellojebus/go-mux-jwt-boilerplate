36e4e9fde3fc7bb08a24d7e725ae648b104b23f3
Jesus Lizama <dev@hellojeb.us>
2018-06-02 19:14:36 -0700

    return after error reponse

--- httpUtils.go
+++ httpUtils.go
@@ -25,6 +25,7 @@ func NewErrorResponse(w http.ResponseWriter, statusCode int, response string){
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(&error)
+	return
 }
 
 
