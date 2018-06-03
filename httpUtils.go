f24b2a1149eb10767f6d6951d05970bd13435c2c
Jesus Lizama <dev@hellojeb.us>
2018-06-02 18:33:13 -0700

    add jwt token verification to middleware

--- httpUtils.go
+++ httpUtils.go
@@ -3,6 +3,8 @@ package main
 import (
 	"net/http"
 	"encoding/json"
+	"github.com/dgrijalva/jwt-go"
+	"os"
 )
 
 type ErrorResponse struct {
@@ -18,4 +20,15 @@ func NewErrorResponse(w http.ResponseWriter, statusCode int, response string){
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(&error)
+}
+
+func verifyToken(tokenString string) (jwt.Claims, error) {
+	signingKey := []byte(os.Getenv("JWT_SECRET"))
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return signingKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token.Claims, err
 }
--- routerMiddleware.go
+++ routerMiddleware.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"strings"
 )
 
 func jwtMiddleware(next http.Handler) http.Handler {
@@ -9,6 +10,12 @@ func jwtMiddleware(next http.Handler) http.Handler {
 		tokenString := r.Header.Get("Authorization")
 		if len(tokenString) == 0 {
 			NewErrorResponse(w, http.StatusUnauthorized, "Authentication failure")
+			return
+		}
+		tokenString = strings.Replace(tokenString, "Bearer ", "", 1)
+		_, err := verifyToken(tokenString)
+		if err != nil {
+			NewErrorResponse(w, http.StatusUnauthorized, "Error verifying JWT token: " + err.Error())
 			return
 		}
 		next.ServeHTTP(w, r)
