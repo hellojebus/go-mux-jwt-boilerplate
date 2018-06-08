83488d06d0f9178aa6ca63b4e356233ee39165ef
Jesus Lizama <dev@hellojeb.us>
2018-06-07 22:23:53 -0700

    add http package

---
+++ http/jwt.go
@@ -0,0 +1,21 @@
+package http
+
+import (
+	"github.com/dgrijalva/jwt-go"
+	"os"
+)
+
+/*
+Verify JWT token based on environment var (JWT_SECRET)
+*/
+
+func VerifyToken(tokenString string) (jwt.Claims, error) {
+	signingKey := []byte(os.Getenv("JWT_SECRET"))
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return signingKey, nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return token.Claims, err
+}
--- http/response.go
+++ http/response.go
@@ -1,10 +1,8 @@
-package main
+package http
 
 import (
 	"net/http"
 	"encoding/json"
-	"github.com/dgrijalva/jwt-go"
-	"os"
 )
 
 type ErrorResponse struct {
@@ -26,20 +24,4 @@ func NewErrorResponse(w http.ResponseWriter, statusCode int, response string){
 	w.WriteHeader(statusCode)
 	json.NewEncoder(w).Encode(&error)
 	return
-}
-
-
-/*
-Verify JWT token based on environment var (JWT_SECRET)
-*/
-
-func verifyToken(tokenString string) (jwt.Claims, error) {
-	signingKey := []byte(os.Getenv("JWT_SECRET"))
-	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
-		return signingKey, nil
-	})
-	if err != nil {
-		return nil, err
-	}
-	return token.Claims, err
 }
