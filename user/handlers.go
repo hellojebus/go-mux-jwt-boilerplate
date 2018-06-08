e4f7fdf0c646959d649c72074414a00d954f7717
Jesus Lizama <dev@hellojeb.us>
2018-06-07 22:24:53 -0700

    refactor user into own model

--- user/handlers.go
+++ user/handlers.go
@@ -1,1 +1,85 @@
 package user
+
+import (
+	"net/http"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"github.com/hellojebus/go-envoz-api/db"
+	customHTTP "github.com/hellojebus/go-envoz-api/http"
+)
+
+func UsersIndexHandler(w http.ResponseWriter, r *http.Request) {
+	var users []User
+	//since we're passing a pointer to users, db.Find assigns array to the address
+	db.DB.Find(&users)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func UsersShowHandler(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user User
+	db.DB.First(&user, params["userId"])
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(user)
+}
+
+func UsersCreateHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	user.Email = r.FormValue("email")
+	user.Name = r.FormValue("name")
+	//get password hash
+	user.Hash = user.hashPassword(r.FormValue("password"))
+	err := db.DB.Create(&user).Error
+	if err != nil {
+		customHTTP.NewErrorResponse(w, http.StatusUnauthorized, "Error: " + err.Error())
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(&user)
+}
+
+func UsersLoginHandler(w http.ResponseWriter, r *http.Request) {
+	var user User
+	db.DB.Where("email = ?", r.FormValue("email")).Find(&user)
+	w.Header().Set("Content-Type", "application/json")
+	if user.checkPassword(r.FormValue("password")) {
+		token, err := user.generateJWT()
+		if err != nil {
+			customHTTP.NewErrorResponse(w, http.StatusUnauthorized, "Error: " + err.Error())
+			return
+		}
+		json.NewEncoder(w).Encode(&token)
+	} else {
+		customHTTP.NewErrorResponse(w, http.StatusUnauthorized, "Password incorrect")
+		return
+	}
+}
+
+func UsersDelete(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	var user User
+	var users []User
+
+	db.DB.First(&user, params["userId"])
+	db.DB.Delete(&user)
+
+	db.DB.Find(&users)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(users)
+}
+
+func UsersUpdate(w http.ResponseWriter, r *http.Request){
+	params := mux.Vars(r)
+	var user User
+	reqUserId := r.Header.Get("userId")
+
+	w.Header().Set("Content-Type", "application/json")
+	if(params["userId"] != reqUserId){
+		customHTTP.NewErrorResponse(w, http.StatusUnauthorized, "Not allowed to edit other users")
+		return
+	}
+	db.DB.First(&user, params["userId"])
+	db.DB.Model(&user).Update("name", r.FormValue("name"))
+	json.NewEncoder(w).Encode(&user)
+}
--- user/routes.go
+++ user/routes.go
@@ -1,1 +1,51 @@
 package user
+
+import "github.com/hellojebus/go-envoz-api/router"
+
+var Routes = router.RoutePrefix{
+	"/users",
+	[]router.Route{
+		router.Route{
+			"UsersIndex",
+			"GET",
+			"",
+			UsersIndexHandler,
+			false,
+		},
+		router.Route{
+			"UsersShow",
+			"GET",
+			"/{userId}",
+			UsersShowHandler,
+			true,
+		},
+		router.Route{
+			"UsersCreate",
+			"POST",
+			"",
+			UsersCreateHandler,
+			false,
+		},
+		router.Route{
+			"UsersLogin",
+			"POST",
+			"/login",
+			UsersLoginHandler,
+			false,
+		},
+		router.Route{
+			"UsersDelete",
+			"DELETE",
+			"/{userId}",
+			UsersDelete,
+			true,
+		},
+		router.Route{
+			"UsersUpdate",
+			"PUT",
+			"/{userId}",
+			UsersUpdate,
+			true,
+		},
+	},
+}
