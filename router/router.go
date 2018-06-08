b9cd7917beae02a746773c0f2e990cbc6bc3e04f
Jesus Lizama <dev@hellojeb.us>
2018-06-07 22:24:25 -0700

    refactor router mechanism

--- router/router.go
+++ router/router.go
@@ -1,1 +1,18 @@
 package router
+
+import "net/http"
+
+var AppRoutes []RoutePrefix
+
+type RoutePrefix struct {
+	Prefix string
+	SubRoutes []Route
+}
+
+type Route struct {
+	Name string
+	Method string
+	Pattern string
+	HandlerFunc http.HandlerFunc
+	Protected bool
+}
